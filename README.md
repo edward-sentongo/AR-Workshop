6dcb7cdd6b2b3cdae989891b22431b1abf97f9fa
Edward Sentongo <edward.sentongo@philips.com>
2018-12-03 20:27:10 +0100

    node successfully placed

--- README.md
+++ README.md
@@ -1,35 +1,35 @@
 # AR-Workshop:  Part 2
 
 1. Theory:
-Node:
-Transformable Nodes:
-3d objects: OBJ, SFX, SFB etc
-Model:
-Model Renderable:
-Pose:
-Anchor:
+    Node:
+    Transformable Nodes:
+    3d objects: OBJ, SFX, SFB etc
+    Model:
+    Model Renderable:
+    Pose:
+    Anchor:
 
 2. Import objects
-@Project:
-app -> sampledata -> models
-main -> assets 
-sfb, sfa
+    @Project:
+        app -> sampledata -> models
+        main -> assets 
+                sfb, sfa
 
-@Poly: https://poly.google.com/
-"cow", unzip obj, mtl and png to sampledata -> models.
+    @Poly: https://poly.google.com/
+    "cow", unzip obj, mtl and png to sampledata -> models.
 
 
 
-Sources of 3d models: 
-- https://poly.google.com/ [100% commercial and guaranteed compartibility]
-- https://clara.io/scenes
-- https://www.yobi3d.com/
-- https://pinshape.com/
-- https://free3d.com/
+    Sources of 3d models: 
+    - https://poly.google.com/ [100% commercial and guaranteed compartibility]
+    - https://clara.io/scenes
+    - https://www.yobi3d.com/
+    - https://pinshape.com/
+    - https://free3d.com/
 
 3. Manipulate object characteristics:
-SFB - Materials[metallic, roughness, opacity, baseColorTint]
-SFB - Models [scale]
+    SFB - Materials[metallic, roughness, opacity, baseColorTint]
+    SFB - Models [scale]
 
 
 Run it on your phone or simulator and scene the scene being created automatically for you
--- app/src/main/java/com/sentongoapps/arworkshop/MainActivity.java
+++ app/src/main/java/com/sentongoapps/arworkshop/MainActivity.java
@@ -1,9 +1,26 @@
 package com.sentongoapps.arworkshop;
 
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Trackable;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +32,65 @@ public class MainActivity extends AppCompatActivity {
 
         fragment = (ArFragment)
                 getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+
+        fragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                addObject(Uri.parse("Cow.sfb"));
+            }
+        });
+    }
+
+    //Get center of fragment. This center is center_of_wherever_ur_pointing
+    private android.graphics.Point getScreenCenter() {
+        View vw = findViewById(android.R.id.content);
+        return new android.graphics.Point(vw.getWidth()/2, vw.getHeight()/2);
+    }
+
+    //1. Add Object to fragment
+    private void addObject(Uri model) {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter(); //See up ;)
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    placeObject(fragment, hit.createAnchor(), model);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    //2. Place object on fragment
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri model) {
+        CompletableFuture<Void> renderableFuture =
+                ModelRenderable.builder()
+                        .setSource(fragment.getContext(), model)
+                        .build()
+                        .thenAccept(renderable -> addNodeToScene(fragment, anchor, renderable))
+                        .exceptionally((throwable -> {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setMessage(throwable.getMessage())
+                                    .setTitle("Sogeti AR Error!");
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                            return null;
+                        }));
+
+        //Todo: you can later use the 'renderableFuture' for other fun stuff
+    }
+
+    //3. Now add it to AR Fragment
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        Node animalNode = new Node();
+        animalNode.setRenderable(renderable);
+        animalNode.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
     }
 }
